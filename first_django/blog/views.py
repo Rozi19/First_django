71da14d4cb3116bb5e76557f583236a67a8df4f5
Roza Alene <rozaalene@gmail.com>
2023-11-11 10:20:00 +0300

    remove the dami data & use data from the data_base

--- first_django/blog/views.py
+++ first_django/blog/views.py
@@ -1,26 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Post
 
-
-posts = [
-    {
-        'title': 'Blog post 1',
-        'author': 'Roza',
-        'content': 'my first Blog',
-        'posted_date': 'Nov 06 2023'
-    },
-    {
-        'title': 'Blog post 2',
-        'author': 'Mihret',
-        'content': 'I am Mihret Tesema, I am so happy to be here',
-        'posted_date': 'Nov 01 2023'
-    }
-]
 
 def home(request):
 
     context = {
-            'posts': posts
+            'posts': Post.objects.all()
         }
     return render(request, 'blog/home.html', context)
 
